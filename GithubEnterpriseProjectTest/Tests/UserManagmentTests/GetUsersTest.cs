ede2e9451c1554303f192858ea5b74109b0258e3
Nyerges Dominik <dommi7797@gmail.com>
2020-12-17 14:35:42 +0100

    added tests

---
+++ GithubEnterpriseProjectTest/Tests/UserManagmentTests/GetUsersTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GithubEnterpriseProjectTest.Tests.UserManagmentTests
+{
+    class GetUsersTest
+    {
+        [Test]
+        public void isUserSavedTest()
+        {
+        }
+    }
+}
