da849dab7a29a5ec34f28898592c2b073614e118
Nyerges Dominik <dommi7797@gmail.com>
2021-01-13 10:09:55 +0100

    bugfix

--- GithubJobsEnterpriseProject/Services/JsonHandlerService.cs
+++ GithubJobsEnterpriseProject/Services/JsonHandlerService.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
 using GithubJobsEnterpriseProject.Models;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace GithubJobsEnterpriseProject.Services
 {
     public class JsonHandlerService
     {
-        private const string _JSONPATH = "users.json";
+        private string _path;
         private IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+
+        IConfiguration _iconfig;
+        public JsonHandlerService(IConfiguration iconfig)
+        {
+            _iconfig = iconfig;
+        }
+
+        public string GetFilename()
+        {
+            return _iconfig.GetValue<string>("UsersPath:Filename");
+        }
 
         public List<User> DeconvertUsersJson()
         {
-            if (isoStore.FileExists(_JSONPATH))
+
+            _path = GetFilename();
+
+            if (isoStore.FileExists(_path))
             {
                 Console.WriteLine("The file already exists!");
-                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_JSONPATH, FileMode.Open, isoStore))
+                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_path, FileMode.Open, isoStore))
                 {
                     using (StreamReader reader = new StreamReader(isoStream))
                     {
@@ -45,9 +60,12 @@ namespace GithubJobsEnterpriseProject.Services
 
         public void ConvertUserToJson(User user)
         {
-            if (isoStore.FileExists(_JSONPATH))
+
+            _path = GetFilename();
+
+            if (isoStore.FileExists(_path))
             {
-                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_JSONPATH, FileMode.Append, isoStore))
+                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_path, FileMode.Append, isoStore))
                 {
                     using (StreamWriter writer = new StreamWriter(isoStream))
                     {
@@ -55,9 +73,9 @@ namespace GithubJobsEnterpriseProject.Services
                     }
                 }
             }
-            else if (!isoStore.FileExists(_JSONPATH))
+            else if (!isoStore.FileExists(_path))
             {
-                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_JSONPATH, FileMode.CreateNew, isoStore))
+                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(_path, FileMode.CreateNew, isoStore))
                 {
                     using (StreamWriter writer = new StreamWriter(isoStream))
                     {
