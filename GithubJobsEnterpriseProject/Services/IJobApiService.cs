abaa95f0ee73cd46c3a61a471347b3db538bb189
Zsani <horvathzsanett.95@gmail.com>
2021-02-23 13:22:47 +0100

    Deleted unnecesarry interface => IJobContext

--- GithubJobsEnterpriseProject/Services/IJobApiService.cs
+++ GithubJobsEnterpriseProject/Services/IJobApiService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GithubJobsEnterpriseProject.Services
 {
-    interface IJobApiService
+    public interface IJobApiService
     {
         public Appsettings GetLink();
         public IEnumerable<GithubJob> GetGithubJobsFromUrl();
