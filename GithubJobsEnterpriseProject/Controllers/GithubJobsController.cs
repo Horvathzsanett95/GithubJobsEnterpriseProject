5b0df5220c31ac189d97a142d7239e84c417dccf
Zsani <horvathzsanett.95@gmail.com>
2021-02-26 08:36:48 +0100

    Deleted unncessarry comments and imports included frontend too

--- GithubJobsEnterpriseProject/Controllers/GithubJobsController.cs
+++ GithubJobsEnterpriseProject/Controllers/GithubJobsController.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace GithubJobsEnterpriseProject.Controllers
